ca290684868e31b9ccb9bed27ce80a3d481024ba
SeoMiYoung <seo20seo00@naver.com>
2025-06-26 16:35:09 +0900

    [chore] constructor 연습

--- src/main/java/com/apple/online_shop/OnlineShopApplication.java
+++ src/main/java/com/apple/online_shop/OnlineShopApplication.java
@@ -6,9 +6,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OnlineShopApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineShopApplication.class, args);
+		var p1 = new Friend("kim");
+		var p2 = new Friend("park");
+		System.out.println(p1.name);
+		System.out.println(p2.name);
 
 	}
 
+}
+
+class Friend {
+	String name = "seo";
+	int age = 26;
+	Friend(String 구멍) { // constructor(object 뽑을 때 초기값 생성)
+		// new Friend()할 때 자동 생성됨
+		this.name = 구멍;
+	}
 }
