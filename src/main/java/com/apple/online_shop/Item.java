7ea5eb0e2d97b5932ff734c3c6681d52332ad30e
SeoMiYoung <seo20seo00@naver.com>
2025-06-28 02:20:17 +0900

    [refactor] 변수에 public 붙이면 다른 모든 클래스에서 문제없이 사용가능

--- src/main/java/com/apple/online_shop/Item.java
+++ src/main/java/com/apple/online_shop/Item.java
@@ -8,7 +8,7 @@ import jakarta.persistence.Id;
 @Entity // 테이블 만들고 싶으면... JPA에선 @Entity 클래스 만들면 자동생성됩니다.
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // id 컬럼에는 이거 붙이라고 강요함
-    Long id;  // 자동으로 증가시켜줌
-    String title;
-    Integer price;
+    public Long id;  // 자동으로 증가시켜줌
+    public String title;
+    public Integer price;
 }
