d98c820a6336c545988d71000623665aa9bb1f0b
SeoMiYoung <seo20seo00@naver.com>
2025-06-28 01:48:33 +0900

    [feat] Item 테이블 생성해보기

--- src/main/java/com/apple/online_shop/Item.java
+++ src/main/java/com/apple/online_shop/Item.java
@@ -1,8 +1,14 @@
 package com.apple.online_shop;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-@Entity
+@Entity // 테이블 만들고 싶으면... JPA에선 @Entity 클래스 만들면 자동생성됩니다.
 public class Item {
-
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // id 컬럼에는 이거 붙이라고 강요함
+    Long id;  // 자동으로 증가시켜줌
+    String title;
+    Integer price;
 }
