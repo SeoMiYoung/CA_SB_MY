d953d2384011e2322d2515c70ae891f420394c97
SeoMiYoung <seo20seo00@naver.com>
2025-06-28 02:24:57 +0900

    [refactor] @Column을 사용해서 제약조건 추가 가능

--- src/main/java/com/apple/online_shop/Item.java
+++ src/main/java/com/apple/online_shop/Item.java
@@ -1,14 +1,13 @@
 package com.apple.online_shop;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity // 테이블 만들고 싶으면... JPA에선 @Entity 클래스 만들면 자동생성됩니다.
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // id 컬럼에는 이거 붙이라고 강요함
     public Long id;  // 자동으로 증가시켜줌
+
+    @Column(nullable = false) // @Column()으로 컬럼에 제약설정 가능
     public String title;
     public Integer price;
 }
