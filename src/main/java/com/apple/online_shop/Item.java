4fd93c6e3be521c1be8a4af1b3cbe9094ffda157
SeoMiYoung <seo20seo00@naver.com>
2025-06-28 02:27:47 +0900

    [refactor] 사실 @Column을 추가해도 이미 만들어진 테이블에 반영이 잘 안됨. 처음부터 제대로 설정하는게 나음.

--- src/main/java/com/apple/online_shop/Item.java
+++ src/main/java/com/apple/online_shop/Item.java
@@ -7,7 +7,6 @@ public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // id 컬럼에는 이거 붙이라고 강요함
     public Long id;  // 자동으로 증가시켜줌
 
-    @Column(nullable = false) // @Column()으로 컬럼에 제약설정 가능
     public String title;
     public Integer price;
 }
