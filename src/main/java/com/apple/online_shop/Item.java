d420deda09e9ea3f7a3bf25dc5c2ba8d32d3d6f1
SeoMiYoung <seo20seo00@naver.com>
2025-06-30 00:19:11 +0900

    [feat] .toString()만들거나, lombok의 @ToString 써서 object 구성 한 눈에 보기

--- src/main/java/com/apple/online_shop/Item.java
+++ src/main/java/com/apple/online_shop/Item.java
@@ -1,12 +1,20 @@
 package com.apple.online_shop;
 
 import jakarta.persistence.*;
+import lombok.ToString;
+
 
 @Entity // 테이블 만들고 싶으면... JPA에선 @Entity 클래스 만들면 자동생성됩니다.
+@ToString
 public class Item {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // id 컬럼에는 이거 붙이라고 강요함
     public Long id;  // 자동으로 증가시켜줌
 
     public String title;
     public Integer price;
+
+    // @ToString으로 대체 (lombok)
+    //public String toString() {
+        //return this.title + this.price;
+    //}
 }
--- src/main/java/com/apple/online_shop/ItemController.java
+++ src/main/java/com/apple/online_shop/ItemController.java
@@ -19,8 +19,11 @@ public class ItemController {
     @GetMapping("/list")
     String show_list(Model model) {
         List<Item> result = itemRepository.findAll();  // 테이블의 모든 데이터를 가져다 줌
-
         model.addAttribute("items", result); // items라는 이름으로 result 정보 보내기
+
+        var a = new Item();
+        System.out.println(a.toString()); // Item(id=null, title=null, price=null)
+
         return "list.html"; // @ResponseBody는 기본적으로 문자열 또는 JSON 형태로 응답을 보내야 함.
     }
 }
